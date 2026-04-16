872f1f097d5bd3b7c2284adec045f3af2f1b5e75
Kolan4ikkkk <andrievskiy.kolia@gmail.com>
2026-04-16 14:18:33 +0300

    first commit

--- n1051_height_checker.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def heightChecker(self, heights):
-        expected = sorted(heights)
-        mismatch = 0
-        for i in range(len(heights)):
-            if heights[i] != expected[i]:
-                mismatch += 1
-        return mismatch
--- n1460_make_two_arrays_equal_by_reversing_subarrays.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def canBeEqual(self, target, arr):
-        """
-        :type target: List[int]
-        :type arr: List[int]
-        :rtype: bool
-        """
-        return sorted(target) == sorted(arr)
